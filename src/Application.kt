7aa361a8bd8df51027eaea03d7d578110300c687
deepan <deepan.elango@spectrum.global>
2021-06-06 01:02:28 +0530

    Initialized GraphQl

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,20 @@
 package com.notes
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import com.apurebase.kgraphql.GraphQL
+import io.ktor.application.Application
+import io.ktor.application.install
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(GraphQL) {
+        playground = true
+        schema {
+            query("hello") {
+                resolver { -> "world" }
+            }
+        }
+    }
 }
-
